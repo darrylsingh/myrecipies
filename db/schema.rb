a7ac05bea126e4e21f548054f34549b53446b246
Darryl Singh <dsingh@ebaraintl.com>
2015-06-30 22:15:53 +0000

    Create Chef & Recipe tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630211927) do
+ActiveRecord::Schema.define(version: 20150630220802) do
+
+  create_table "chefs", force: true do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
